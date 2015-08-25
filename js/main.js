497f10ea89bccd6f075624a173dbf1d7e7269492
carlou5 <carlos52292@hotmail.com>
2015-08-25 19:13:00 -0400

    add scrollspy collapse and progress

--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
-$(function(){
-  $('.containermix').mixItUp();
-});
-
-
 $(document).ready(function(){
+	$('.containermix').mixItUp();
     // Target your .container, .wrapper, .post, etc.
     $("#video-container").fitVids();
-});
 
-$(document).ready(function() {
 	$(".fancy_title").lettering();
+
+	$("#fittext").fitText();
+
+
+	$('a[href^="#"]').click(function(e) {
+	    // Prevent the jump and the #hash from appearing on the address bar
+	    e.preventDefault();
+	    // Scroll the window, stop any previous animation, stop on user manual scroll
+	    // Check https://github.com/flesler/jquery.scrollTo for more customizability
+	    console.log(this.hash);
+	    $(window).stop(true).scrollTo(this.hash, {duration:1000, interrupt:true,easing:'easeInBack'});
+	});
+
+
 });
 
-$(document).ready(function() {
-	$("#fittext").fitText();
-});
+
+
+
